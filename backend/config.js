c27727b80fade50faeb7f06afdf71bd5e11dbfd9
Gallahaad <professional.utkarshmisra@gmail.com>
2023-12-23 01:06:34 +0530

    Git Repo Initialized

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5500;
+
+export const mongoDBURL =
+  "mongodb+srv://root:rootadmin@mern-book-store.vq5y7cd.mongodb.net/books-data?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Book Store");
+});
+
+app.use("/books", booksRoute);
+
+//middleware to use CORS policy
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
